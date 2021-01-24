b0f5337e6a5a6834c241ecfd0782e6858b9ba99c
Emiliano Gatti <emiliano.gatti@gmail.com>
2021-01-24 12:55:03 -0300

    Changes to be committed: 	modified:   app.js 	deleted:    public/css/styles.css 	modified:   views/home.html    actualizacion por puesta al día

--- app.js
+++ app.js
@@ -8,27 +8,9 @@ app.listen (3123, () => {
 });/*
 app.get('/turing', function (req, res){
     res.sendFile(path.join(__dirname, './views/turing.html'));
-});
-app.get('/lovelace', function (req, res){
-    res.sendFile(path.join(__dirname, './views/lovelace.html'));
-});
-app.get('/hopper', function (req, res){
-    res.sendFile(path.join(__dirname, './views/hopper.html'));
-});
-app.get('/hamilton', function (req, res){
-    res.sendFile(path.join(__dirname,'./views/hamilton.html'));
-});
-app.get('/clarke', function (req, res){
-    res.sendFile(path.join(__dirname, './views/clarke.html'));
-});
-app.get('/berners-lee', function (req, res){
-    res.sendFile(path.join(__dirname, './views/berners-lee.html'));
-});
-app.get('/babbage', function (req, res){
-    res.sendFile(path.join(__dirname, './views/babbage.html'));
-});*/
+*/
 app.get('/home', function (req, res){
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    res.sendFile(path.join(__dirname, './views/home.html'));
 });
 app.get('/', function (req, res){
     res.send('Saludos Terricola')
